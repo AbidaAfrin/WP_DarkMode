ea2feaa5795b9afcc2ae3828cc33fdab69f48943
Abida Afrin <afrin.abida29@gmail.com>
2024-09-19 19:48:38 +0600

    initial commit

--- src/test/java/pages/LogInPage.java
+++ src/test/java/pages/LogInPage.java
@@ -5,12 +5,11 @@ import org.openqa.selenium.By;
 
 public class LogInPage extends BasePage{
 
-//    Dotenv dotenv = Dotenv.load();
-//    public String username =dotenv.get("username");
-//    public String password =dotenv.get("password");
+    Dotenv dotenv = Dotenv.load();
+    public String username =dotenv.get("username");
+    public String password =dotenv.get("password");
 
-    public String username ="admin";
-    public String password ="admin@12345678";
+    
 
     public By usernameInputBox = By.id("user_login");
     public By passwordInputBox = By.id("user_pass");
